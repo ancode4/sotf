1f9f20afafb5f64cfd2de6d9920ec7d0381f4413
Anish Hirlekar <anishsaas@gmail.com>
2022-12-05 01:20:52 +0530

    Added tabs, projects get API request

--- src/admin-page.jsx
+++ src/admin-page.jsx
@@ -1,4 +1,5 @@
-import ForgeUI, {AdminPage, render, Text} from '@forge/ui';
+import ForgeUI, {AdminPage, render, Text, Heading, Tabs, Tab, 
+        useProductContext, useState} from '@forge/ui';
 
 const ConfigurePage = () => {
     return (
@@ -9,9 +10,26 @@ const ConfigurePage = () => {
 };
 
 const MainAdminPage = () => {
+    const projects = useState(async ()=> await getProjects() );
+    console.log("PROJECT:");
+    console.log(JSON.stringify(projects, null, 2))
+
+    let tabs = []
+
+    for(let i=0;i<projects.length;i++){
+        let project = projects[i];
+        console.log(JSON.stringify(project.name))
+        tabs.push(<Tab label="{project.key}">
+                    <Text>{project.name}</Text>
+                </Tab>)
+    }
+
     return (
         <AdminPage>
-            <Text>Hello Admin!</Text>
+            <Heading size="medium">Site statistics</Heading>
+            <Tabs>
+                {tabs}
+            </Tabs>
         </AdminPage>
     );
 };
@@ -19,3 +37,11 @@ const MainAdminPage = () => {
 export const runAdminPage = render(
     <MainAdminPage />
 );
+
+const getProjects = async (issueId) => {
+  const res = await api
+    .asUser()
+    .requestJira(`/rest/api/3/project/search`);
+    const data = await res.json();
+    return data.values;
+};
