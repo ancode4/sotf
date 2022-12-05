4b710aea3a2dde157d130215e23898fd92c1d8ed
Anish Hirlekar <anishsaas@gmail.com>
2022-12-05 19:26:30 +0530

    Added issue type count and issue type schemes count

--- src/admin-page.jsx
+++ src/admin-page.jsx
@@ -1,32 +1,73 @@
-import ForgeUI, {AdminPage, render, Text, Heading, Tabs, Tab, 
-        useProductContext, useState} from '@forge/ui';
+import ForgeUI, {AdminPage, render, Text, Heading, 
+        useProductContext, useState, Table, Head, Row, Cell} from '@forge/ui';
 
-const ConfigurePage = () => {
-    return (
-        <AdminPage>
-            <Text>Hello, world!</Text>
-        </AdminPage>
-    );
-};
 
 const MainAdminPage = () => {
     const projects = useState(async ()=> await getProjects() );
     const values = projects[0].values;
-    let tabs = []
-
+    
+    const projectRows = []
     for(let i=0;i<values.length;i++){
-        let value = values[i];
-        tabs.push(<Tab label={value.name}>
-                    <Text>{value.name}</Text>
-                </Tab>)
+        let project = values[i];
+        projectRows.push(
+            <Row>       
+                <Cell>
+                    <Text>{i+1}</Text>
+                </Cell>
+                <Cell>
+                    <Text>{project.key}</Text>
+                </Cell>
+                <Cell>
+                    <Text>{project.name}</Text>
+                </Cell>
+            </Row>
+        )
     }
+
+    const issueTypes = useState(async ()=> await getIssueTypes() );
+
+    const issueTypesSchemes = useState(async ()=> await getIssueTypesSchemes() );
+    //console.log("ISSUE TYPE SCHEMES:")
+    //console.log(issueTypesSchemes)
 
     return (
         <AdminPage>
             <Heading size="medium">Site statistics</Heading>
-            <Tabs>
-                {tabs}
-            </Tabs>
+            <Heading size="small">Projects</Heading>
+            <Table>
+                <Head>
+                    <Cell>
+                        <Text>Sr.</Text>
+                    </Cell>
+                    <Cell>
+                        <Text>Key</Text>
+                    </Cell>
+                    <Cell>
+                        <Text>Name</Text>
+                    </Cell>
+                </Head>
+                {projectRows}
+            </Table>
+
+            <Heading size="small">Issue Types</Heading>
+            <Table>
+                <Row>
+                    <Cell>
+                        <Text>Number of issue types</Text>
+                    </Cell>
+                    <Cell>
+                        <Text>{issueTypes[0].length}</Text>
+                    </Cell>
+                </Row>
+                <Row>
+                    <Cell>
+                        <Text>Number of issue types schemes</Text>
+                    </Cell>
+                    <Cell>
+                        <Text>{issueTypesSchemes[0].total}</Text>
+                    </Cell>
+                </Row>
+            </Table>
         </AdminPage>
     );
 };
@@ -35,7 +76,23 @@ export const runAdminPage = render(
     <MainAdminPage />
 );
 
-const getProjects = async (issueId) => {
+const getIssueTypes = async () => {
+  const res = await api
+    .asUser()
+    .requestJira(`/rest/api/3/issuetype`);
+    const data = await res.json();
+    return data;
+};
+
+const getIssueTypesSchemes = async () => {
+  const res = await api
+    .asUser()
+    .requestJira(`/rest/api/3/issuetypescheme`);
+    const data = await res.json();
+    return data;
+};
+
+const getProjects = async () => {
   const res = await api
     .asUser()
     .requestJira(`/rest/api/3/project/search`);
